0204888b9a3d2b759235d8d6b2b28ef1d3ca193b
jay2034 <57559104+jay2034@users.noreply.github.com>
2020-01-18 19:59:08 +0900

    200118

---
+++ 200118/logical.py
@@ -0,0 +1,20 @@
+print(3>2)
+print(5>7)
+print( 3>2 and 5>7)
+print( 9<3 and 0>29)
+print( 3<4 or 9>1)
+print( not(7<4 and 0<5) )
+
+print(3%44 == 3 and 8*3 == 24)
+# print(3%44 and 8*3)
+# print('cheese' and 'burger')
+# print(2+2*2<4 or 3+4*2>20)
+# print( not(1%4*3%4<9 and 2*3%43*5>99))
+print( 5*55==225 or 999%55==22)
+
+print("========================")
+
+print( not(7 // 5 == 2) or (900 % 10 != 0) )
+print("apple" + "juice" == "applejuice")
+
+print( not(((3>5) and not(7>9)) == True))
---
+++ 200118/structure.py
@@ -0,0 +1,33 @@
+["bale", "modric","messi","kroos"]
+print(  ["bale", "modric","messi","kroos"] )
+print(  ["bale", "modric","messi","kroos"][2] + " is my favorite player." )
+[2+2*2,"cheese"+"burger",3*3+4==13,"soccer"+"player",9-8*2%2]
+print([2+2*2,"cheese"+"burger",3*3+4==13,"soccer"+"player",9-8*2%2][2])
+
+# {
+# "name" : "Elsa",
+# "Age" : 21,
+# "married" : False
+# }
+
+print({
+"name" : "Elsa",
+"Age" : 21,
+"married" : False
+}["Age"] + 30 )
+
+# {
+# "name" : "bale"
+# "age" : 32,
+# "team" : "real madrid",
+# "rival team" : "barcelona",
+# "married" : True
+# }
+
+print({
+"name" : "Bale",
+"age" : 32,
+"team" : "Real Madrid",
+"rival team" : "Barcelona",
+"married" : True
+}["rival team"] or False)
